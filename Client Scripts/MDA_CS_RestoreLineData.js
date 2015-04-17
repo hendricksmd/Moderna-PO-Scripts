0c5f5510aa99125fe67f924e2312b2e8e38794a8
Matt Hendricks <mhendricks@outlook.com>
2015-04-17 09:43:29 -0600

    comment updates

--- Client Scripts/MDA_CS_RestoreLineData.js
+++ Client Scripts/MDA_CS_RestoreLineData.js
@@ -7,36 +7,22 @@
  *If a user has to change the item on a PO, the previously committed values are captured
  *and filled back in after the new item post sourcing event.
  */
-
-/**
- * Module Description
- * 
- * Version    Date            Author           Remarks
- * 1.00       16 Apr 2015     McGladrey
- *
- */
-
-
-/**
- * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
- * @appliedtorecord purchaseorder
- * 
- * @param {String} type Sublist internal id
- * @param {String} name Field internal id
- * @returns {Void}
- */
 function postSource_restoreLine(type, name) {
+	/*
+	 * if the item field on the item sublist is changed (on a previously committed line)
+	 * then execute
+	 */
 	if (type == 'item' && name == 'item') {
 		
 		try {
 			//Get committed line values
-			var lineId = nlapiGetCurrentLineItemIndex(type);
-			var	qty = nlapiGetLineItemValue(type, 'quantity', lineId);
-			var	stDescription = nlapiGetLineItemValue(type, 'description', lineId);
-			var	curRate = nlapiGetLineItemValue(type, 'rate', lineId);
-			var	customerId = nlapiGetLineItemValue(type, 'customer', lineId);
+			var lineId = nlapiGetCurrentLineItemIndex(type); 						//get the current line id
+			var	qty = nlapiGetLineItemValue(type, 'quantity', lineId); 				//get quantity
+			var	stDescription = nlapiGetLineItemValue(type, 'description', lineId); //get item description
+			var	curRate = nlapiGetLineItemValue(type, 'rate', lineId); 				//get unit rate
+			var	customerId = nlapiGetLineItemValue(type, 'customer', lineId);		//get customer/project ID
 			
-			//set new values after post sourcing if not null
+			//set new values after item change post sourcing if values not null
 			if (qty!= null) {
 				nlapiSetCurrentLineItemValue(type, 'quantity', qty, false);
 			}
@@ -51,7 +37,7 @@ function postSource_restoreLine(type, name) {
 				nlapiSetCurrentLineItemValue(type, 'customer', projId, false);
 			}
 		} catch (e) {
-			nlapiLogExecution('ERROR', 'unexpected error', e.toString());
+			nlapiLogExecution('ERROR', 'unexpected error restoring previous line values', e.toString());
 		}
 	}
 }
